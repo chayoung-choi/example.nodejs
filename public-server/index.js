2c1ea51263933f925de370dc2c3dcb084b8ee719
chayoung_choi <chayoung.dev@gmail.com>
2020-11-30 13:13:31 +0900

    node test
    node test

--- public-server/index.js
+++ public-server/index.js
@@ -10,6 +10,29 @@ app.get('/', function(req,res){
 	res.sendFile(path.join(__dirname , 'index.html'));
 })
 
+let data = [
+	{
+		id: 1,
+		name: 'Alice'
+	},
+	{
+		id: 2,
+		name: 'Kelly'
+	},
+	{
+		id: 3,
+		name: 'Mant'
+	}
+]
+app.get('/data', function(req, res) {
+	console.log('get', new Date());
+	return res.json(data);
+});
+app.post('/data', function(req, res) {
+	console.log('post', new Date());
+	console.log('req.data', req);
+	return res.json(data);
+});
 var port = 80;
 app.listen(port , ()=>{
 	console.log('Server is running at '+port);
